2e96f61d75193a15d5f99205e4e66b7d45881192
Lauri Knuuttila <lrzhoki@gmail.com>
2025-01-09 18:30:32 +0200

    valmis teht 1

--- teht1/main.cpp
+++ teht1/main.cpp
@@ -4,25 +4,40 @@
 
 using namespace std;
 
+int game(int maxnum);
+
 int main()
 {
+    int maxnum = 0;
+    cout << "Anna maksimiluku: " << endl;
+    cin >> maxnum;
+    int arvaukset = game(maxnum);
+    cout << "Arvauksien maara: " << arvaukset << endl;
+    return 0;
+}
+
+
+
+    int game(int maxnum)
+    {
     srand(time(0));
-    int sluku = rand() % 20 + 1;
-    int kayttaja;
+    int kayttaja = 0;
+    int arvaukset = 0;
+    int sluku = rand() % maxnum + 1;
     while(kayttaja != sluku)
     {
         cout << "Arvaa Luku" << endl;
         cin >> kayttaja;
-            if(kayttaja < sluku)
-            {
+        arvaukset++;
+        if(kayttaja < sluku)
+        {
             cout << "Lukusi on pienempi" << endl;
-            }
-            if(kayttaja > sluku)
-            {
-                cout << "lukusi on suurempi" << endl;
-            }
+        }
+        if(kayttaja > sluku)
+        {
+            cout << "lukusi on suurempi" << endl;
+        }
     }
     cout << "OIKEIN" << endl;
-
-    return 0;
-}
+    return arvaukset;
+    }
