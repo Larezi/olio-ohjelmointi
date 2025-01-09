146082f5b56701744b33f80f41e0bce7c26b741f
Lauri Knuuttila <lrzhoki@gmail.com>
2025-01-09 17:59:52 +0200

    tehtava1_ekaosuus

--- teht1/main.cpp
+++ teht1/main.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    srand(time(0));
+    int sluku = rand() % 20 + 1;
+    int kayttaja;
+    while(kayttaja != sluku)
+    {
+        cout << "Arvaa Luku" << endl;
+        cin >> kayttaja;
+            if(kayttaja < sluku)
+            {
+            cout << "Lukusi on pienempi" << endl;
+            }
+            if(kayttaja > sluku)
+            {
+                cout << "lukusi on suurempi" << endl;
+            }
+    }
+    cout << "OIKEIN" << endl;
+
     return 0;
 }
